f180acd713d5d67979b7dd9f032b65d3a57012fe
Noah Bruns <nbruns@efs.at>
2025-03-11 11:26:46 +0100

    updates to API and docu

--- src/item.rs
+++ src/item.rs
@@ -15,21 +15,65 @@
 // NOTICE: SLIGHT REWORK FROM NOAH BRUNS
 // Shifting index by 1
 
+//!
+//! Item is a unique identifier for an item.
+//!
+//! It is used to represent an item in a transaction.
+//!
+//! The Item is a simple wrapper around a `u32` that represents the unique ID of the item.
+//!
+//! # Examples
+//!
+//! ```
+//! use itemizer::Item;
+//!
+//! let item1 = Item::with_id(0);
+//! let item2 = Item::with_id(1);
+//!
+//! assert_eq!(item1.as_index(), 0);
+//! assert_eq!(item2.as_index(), 1);
+//! ```
+//!
+
 #[derive(Copy, Clone, Hash, PartialOrd, PartialEq, Eq, Ord, Debug)]
 pub struct Item {
     id: u32,
 }
 
 impl Item {
+    /// Creates a new `Item` with the given ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Item;
+    ///
+    /// let item1 = Item::with_id(0);
+    /// let item2 = Item::with_id(1);
+    ///
+    /// assert_eq!(item1.as_index(), 0);
+    /// assert_eq!(item2.as_index(), 1);
+    /// ```
+    ///
     pub fn with_id(id: u32) -> Item {
         Item { id: id }
     }
 
+    /// Returns the ID of the `Item` as a `usize`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Item;
+    ///
+    /// let item1 = Item::with_id(0);
+    /// let item2 = Item::with_id(1);
+    ///
+    /// assert_eq!(item1.as_index(), 0);
+    /// assert_eq!(item2.as_index(), 1);
+    /// ```
+    ///
     pub fn as_index(&self) -> usize {
         self.id as usize
-    }
-
-    pub fn is_null(&self) -> bool {
-        self.id == 0
     }
 }
--- src/itemizer.rs
+++ src/itemizer.rs
@@ -52,6 +52,16 @@ impl<T> Itemizer<T>
 where
     T: Eq + Hash + Ord + Clone,
 {
+    /// Creates a new, empty `Itemizer`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Itemizer;
+    ///
+    /// let itemizer: Itemizer<String> = Itemizer::new();
+    /// assert_eq!(itemizer.len(), 0);
+    ///
     pub fn new() -> Itemizer<T> {
         Itemizer {
             next_item_id: 0,
@@ -60,6 +70,25 @@ where
         }
     }
 
+    /// Returns the `Item` for the given item. If the item is not in the
+    /// `Itemizer`, it is added and a new `Item` is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Itemizer;
+    ///
+    /// let mut itemizer = Itemizer::new();
+    /// let item1 = itemizer.id_of(&"item1".to_string());
+    /// let item2 = itemizer.id_of(&"item2".to_string());
+    /// let item3 = itemizer.id_of(&"item1".to_string());
+    ///
+    /// assert_eq!(item1.as_index(), 0);
+    /// assert_eq!(item2.as_index(), 1);
+    /// assert_eq!(item3.as_index(), 0);
+    /// assert_eq!(itemizer.len(), 2);
+    /// ```
+    ///
     pub fn id_of(&mut self, item: &T) -> Item {
         if let Some(id) = self.item_str_to_id.get(item) {
             return *id;
@@ -72,26 +101,95 @@ where
 
         self.item_id_to_str.push(item.clone());
 
-        assert_eq!(self.item_id_to_str.len(), id as usize);
+        assert_eq!(self.item_id_to_str.len(), (id + 1) as usize);
 
         Item::with_id(id)
     }
 
-    pub fn id_of_exists(&self, item: &T) -> Option<Item> {
+    /// Returns the `Item` for the given item if it exists in the `Itemizer`.
+    /// If the item is not in the `Itemizer`, `None` is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Itemizer;
+    ///
+    /// let mut itemizer = Itemizer::new();
+    /// let item1 = itemizer.id_of(&"item1".to_string());
+    /// let item2 = itemizer.id_of_opt(&"item2".to_string());
+    /// let item3 = itemizer.id_of_opt(&"item1".to_string());
+    ///
+    /// assert_eq!(item1.as_index(), 0);
+    /// assert_eq!(item2, None);
+    /// assert_eq!(item3, Some(item1));
+    /// assert_eq!(itemizer.len(), 1);
+    /// ```
+    ///
+    pub fn id_of_opt(&self, item: &T) -> Option<Item> {
         if let Some(id) = self.item_str_to_id.get(item) {
             return Some(*id);
         } else {
             None
         }
     }
+
+    /// Returns the value of the given `Item`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Itemizer;
+    ///
+    /// let mut itemizer = Itemizer::new();
+    /// let item1 = itemizer.id_of(&"item1".to_string());
+    /// let item2 = itemizer.id_of(&"item2".to_string());
+    ///
+    /// assert_eq!(itemizer.value_of(&item1), &"item1".to_string());
+    /// assert_eq!(itemizer.value_of(&item2), &"item2".to_string());
+    /// ```
+    ///
     pub fn value_of(&self, id: &Item) -> &T {
         &self.item_id_to_str[id.as_index()]
     }
 
+    /// Returns the number of items in the `Itemizer`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Itemizer;
+    ///
+    /// let mut itemizer = Itemizer::new();
+    /// assert_eq!(itemizer.len(), 0);
+    /// itemizer.id_of(&"item1".to_string());
+    /// assert_eq!(itemizer.len(), 1);
+    /// itemizer.id_of(&"item2".to_string());
+    /// assert_eq!(itemizer.len(), 2);
+    /// itemizer.id_of(&"item1".to_string());
+    /// assert_eq!(itemizer.len(), 2);
+    /// ```
+    ///
     pub fn len(&self) -> usize {
         self.item_id_to_str.len()
     }
 
+    /// Returns an iterator over the items in the `Itemizer`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use itemizer::Itemizer;
+    ///
+    /// let mut itemizer = Itemizer::new();
+    /// itemizer.id_of(&"item1".to_string());
+    /// itemizer.id_of(&"item2".to_string());
+    ///
+    /// let mut iter = itemizer.iter();
+    /// assert_eq!(iter.next(), Some(&"item1".to_string()));
+    /// assert_eq!(iter.next(), Some(&"item2".to_string()));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    ///
     pub fn iter(&self) -> Iter<'_, T> {
         self.item_id_to_str.iter()
     }
@@ -103,5 +201,95 @@ impl Debug for Itemizer<String> {
             writeln!(f, "{}: {}", key, value.as_index())?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_itemizer() {
+        let itemizer: Itemizer<String> = Itemizer::new();
+        assert_eq!(itemizer.len(), 0);
+    }
+
+    #[test]
+    fn test_id_of() {
+        let mut itemizer = Itemizer::new();
+        let item1 = itemizer.id_of(&"item1".to_string());
+        let item2 = itemizer.id_of(&"item2".to_string());
+        let item3 = itemizer.id_of(&"item1".to_string());
+
+        assert_eq!(item1.as_index(), 0);
+        assert_eq!(item2.as_index(), 1);
+        assert_eq!(item3.as_index(), 0);
+        assert_eq!(itemizer.len(), 2);
+    }
+
+    #[test]
+    fn test_id_of_opt() {
+        let mut itemizer = Itemizer::new();
+        let item1 = itemizer.id_of(&"item1".to_string());
+        let item2 = itemizer.id_of_opt(&"item2".to_string());
+        let item3 = itemizer.id_of_opt(&"item1".to_string());
+
+        assert_eq!(item1.as_index(), 0);
+        assert_eq!(item2, None);
+        assert_eq!(item3, Some(Item::with_id(0)));
+    }
+
+    #[test]
+    fn test_value_of() {
+        let mut itemizer = Itemizer::new();
+        let item1 = itemizer.id_of(&"item1".to_string());
+        let item2 = itemizer.id_of(&"item2".to_string());
+
+        assert_eq!(itemizer.value_of(&item1), &"item1".to_string());
+        assert_eq!(itemizer.value_of(&item2), &"item2".to_string());
+    }
+
+    #[test]
+    fn test_len() {
+        let mut itemizer = Itemizer::new();
+        assert_eq!(itemizer.len(), 0);
+        itemizer.id_of(&"item1".to_string());
+        assert_eq!(itemizer.len(), 1);
+        itemizer.id_of(&"item2".to_string());
+        assert_eq!(itemizer.len(), 2);
+        itemizer.id_of(&"item1".to_string());
+        assert_eq!(itemizer.len(), 2);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut itemizer = Itemizer::new();
+        itemizer.id_of(&"item1".to_string());
+        itemizer.id_of(&"item2".to_string());
+
+        let mut iter = itemizer.iter();
+        assert_eq!(iter.next(), Some(&"item1".to_string()));
+        assert_eq!(iter.next(), Some(&"item2".to_string()));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_different_types() {
+        let mut itemizer_str: Itemizer<String> = Itemizer::new();
+        let mut itemizer_int: Itemizer<i32> = Itemizer::new();
+
+        let item_str1 = itemizer_str.id_of(&"hello".to_string());
+        let item_str2 = itemizer_str.id_of(&"world".to_string());
+
+        let item_int1 = itemizer_int.id_of(&1);
+        let item_int2 = itemizer_int.id_of(&2);
+
+        assert_eq!(item_str1.as_index(), 0);
+        assert_eq!(item_str2.as_index(), 1);
+        assert_eq!(itemizer_str.len(), 2);
+
+        assert_eq!(item_int1.as_index(), 0);
+        assert_eq!(item_int2.as_index(), 1);
+        assert_eq!(itemizer_int.len(), 2);
     }
 }
